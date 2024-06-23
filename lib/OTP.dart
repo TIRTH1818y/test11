9be19a947e21060191159b3bf95c221bef4dc547
SONIGARA TIRTH <153047795+TIRTH1818y@users.noreply.github.com>
2024-06-23 17:41:03 +0530

    hy

--- lib/OTP.dart
+++ lib/OTP.dart
@@ -1,6 +1,7 @@
-import 'dart:js_interop';
-import 'package:firebase_core/firebase_core.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:test11/otpUI.dart';
 
 class phoneotp extends StatefulWidget {
   const phoneotp({super.key});
@@ -10,45 +11,52 @@ class phoneotp extends StatefulWidget {
 }
 
 class _phoneotpState extends State<phoneotp> {
-
   var phonenumber = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Restore Password"),
-        centerTitle:  true,
+        title: Text("OTP "),
+        centerTitle: true,
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(
+              controller: phonenumber,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  hintText: "+91 add Enter Phone Number",
+                  suffixIcon: Icon(Icons.phone),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  )),
+            ),
+          ),
+          SizedBox(height: 30),
+          ElevatedButton(
+              onPressed: () async {
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: phonenumber,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: "Enter Phone Number",
-                    suffixIcon: Icon(Icons.phone),
-                    border: OutlineInputBorder(
-                      borderRadius:BorderRadius.circular(24),
-                    )
-                  ),
-                ),
-              ),
-          SizedBox(height : 30),
-          ElevatedButton(onPressed:()async{
-
-          }, child: Text("Verify Your Number"))
-            
+                await
+                //allocated phone number in firebase
+                FirebaseAuth.instance.verifyPhoneNumber(
+                    verificationCompleted: (PhoneAuthCredential credential) {},
+                    //jo verification fail thai to ex ni andr Exception jase
+                    verificationFailed: (FirebaseAuthException ex) {},
+                    //verification screen par mokal va mare and jo token expire thai int resendtoken thi
+                    codeSent: (String verificationid, int? resendtoken) {
+                      Navigator.push(context, MaterialPageRoute(builder:(Context)=>otpUI(verificationid: verificationid,) ));
+                    },
+                    //jo OTP expire thai to ("verificationid ")  phonenumber textfield thi pachu OTP mokal va mate
+                    codeAutoRetrievalTimeout: (String verificationid) {},
+                    phoneNumber: phonenumber.text.toString());
+              },
+              child: Text("Verify Your Number"))
         ],
       ),
     );
   }
-}
-
-class FirebaseAuth {
 }
--- lib/otpUI.dart
+++ lib/otpUI.dart
@@ -1,14 +1,18 @@
+import 'dart:math';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:test11/main.dart';
 
 class otpUI extends StatefulWidget {
-  const otpUI({super.key});
+  //send the otp to otp.dart through
+  String verificationid;
+  otpUI({super.key, required this.verificationid});
 
   @override
   State<otpUI> createState() => _otpUIState();
 }
 
 class _otpUIState extends State<otpUI> {
-
   var otpcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -16,7 +20,6 @@ class _otpUIState extends State<otpUI> {
       appBar: AppBar(
         title: Text("OTP"),
       ),
-
       body: Column(
         children: [
           Padding(
@@ -25,18 +28,30 @@ class _otpUIState extends State<otpUI> {
               controller: otpcontroller,
               keyboardType: TextInputType.phone,
               decoration: InputDecoration(
-                hintText: "Enter OTP",
-                suffixIcon: Icon(Icons.phone_rounded),
-                border: OutlineInputBorder(
-
-                )
-
-              ),
+                  hintText: "Enter OTP",
+                  suffixIcon: Icon(Icons.phone_rounded),
+                  border: OutlineInputBorder()),
             ),
           ),
-
           SizedBox(height: 30),
-          ElevatedButton(onPressed:(){}, child: Text("send OTP"))
+          ElevatedButton(
+              onPressed: () async {
+                try {// check otp from send firebase to otpcontroller
+                  PhoneAuthCredential Credential =
+                  await PhoneAuthProvider.credential(verificationId: widget.verificationid, smsCode: otpcontroller.text.toString());
+                    //if condition is true then push the main screen
+                  FirebaseAuth.instance.signInWithCredential(Credential).then((value)
+                  {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => MyHomePage(title: "My home page")));
+                  });
+                } //ex use for any exception headlining
+                catch (ex){
+                  log(ex.toString() as num);
+                }
+
+              },
+              child: Text("send OTP"))
         ],
       ),
     );
